7949d6b2ab1edf4affdee6e616291ec5dd80a727
李巍 <LWnoheart@LWnoheartMacBook-Pro.local>
2017-03-16 15:44:20 +0800

    完善link和两者的点击事件

---
+++ Test_CoreText/model/LWAttributedHyperlink.h
@@ -0,0 +1,13 @@
+//
+//  LWAttributedHyperlink.h
+//  Test_CoreText
+//
+//  Created by 李巍 on 2017/3/16.
+//  Copyright © 2017年 李巍. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LWAttributedHyperlink : NSObject
+
+@end
---
+++ Test_CoreText/model/LWAttributedHyperlink.m
@@ -0,0 +1,13 @@
+//
+//  LWAttributedHyperlink.m
+//  Test_CoreText
+//
+//  Created by 李巍 on 2017/3/16.
+//  Copyright © 2017年 李巍. All rights reserved.
+//
+
+#import "LWAttributedHyperlink.h"
+
+@implementation LWAttributedHyperlink
+
+@end
